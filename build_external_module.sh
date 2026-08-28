52225b7911ca50f4e3b53e59c77674bd26c9f4bf
Mantii04 <dijamantbehlulaj1@gmail.com>
2026-08-28 21:10:54 +0200

    Replace ff_driver with MemKernel

--- build_external_module.sh
+++ build_external_module.sh
@@ -9,12 +9,12 @@ export ANDROID_MAJOR_VERSION=s
 export BUILD_CROSS_COMPILE="${RDIR}/toolchain/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-"
 export BUILD_CC="${RDIR}/toolchain/clang/host/linux-x86/clang-r353983c/bin/clang"
 
-# FIX: Remove the accidental double space in vermagic in ALL config files
+# FIX: Remove the accidental double space in vermagic
 sed -i 's/Diamant  SMP/Diamant SMP/g' "${RDIR}/out/.config"
 sed -i 's/Diamant  SMP/Diamant SMP/g' "${RDIR}/out/include/config/kernel.release"
 sed -i 's/Diamant  SMP/Diamant SMP/g' "${RDIR}/out/include/generated/utsrelease.h"
 
-export MODULE_DIR="${RDIR}/ff_driver"
+export MODULE_DIR="${RDIR}/memkernel"
 
 make -w \
   -C "${RDIR}" \
