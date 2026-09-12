df5526f20bc65131ecb75568442e688343f1c2e5
Mantii04 <dijamantbehlulaj1@gmail.com>
2026-09-12 23:40:00 +0200

    ovo/kkit: force kprobe path for kallsyms_lookup_name (Samsung 4.19)

--- ovo/ovo/kkit.c
+++ ovo/ovo/kkit.c
@@ -63,7 +63,8 @@ bool is_file_exist(const char *filename) {
 }
 
 unsigned long ovo_kallsyms_lookup_name(const char *symbol_name) {
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 7, 0)
+    /* Samsung 4.19 does not export kallsyms_lookup_name to modules.
+     * Force the kprobe-based lookup path unconditionally. */
     typedef unsigned long (*kallsyms_lookup_name_t)(const char *name);
 
     static kallsyms_lookup_name_t lookup_name = NULL;
@@ -72,19 +73,14 @@ unsigned long ovo_kallsyms_lookup_name(const char *symbol_name) {
                 .symbol_name = "kallsyms_lookup_name"
         };
 
-        if(register_kprobe(&kp) < 0) {
+        if (register_kprobe(&kp) < 0) {
             return 0;
         }
 
-        // 高版本一些地址符号不再导出，需要通过kallsyms_lookup_name获取
-        // 但是kallsyms_lookup_name也是一个不导出的内核符号，需要通过kprobe获取
         lookup_name = (kallsyms_lookup_name_t) kp.addr;
         unregister_kprobe(&kp);
     }
     return lookup_name(symbol_name);
-#else
-    return kallsyms_lookup_name(symbol_name);
-#endif
 }
 
 unsigned long *ovo_find_syscall_table(void) {
